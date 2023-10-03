3f978a3a9412e4da0f6117846bccdef7f1285541
tomborc <t.borczynski@autos.com.pl>
2023-10-03 15:04:28 +0200

    add model to manage app_task table in database

--- model/App_task_model.php
+++ model/App_task_model.php
@@ -1,26 +1,31 @@
 <?php
 
-class Department_user_model extends Database_model {
+class App_task_model extends Database_model {
     public function __construct(){
         parent::__construct();
     }
     public function __call($m,$a){
         Throw New \Exception(__METHOD__.'() Method `'.$m.'` not exists in this class `'.__CLASS__.'`!\nMethod call with arguments:\n'.serialize($a),1);
     }
-    public function getUserDepartments(string|int $id_user=0):array{
+    public function getByName(string $name='fsDelete'):array{
         return $this->Main->squery("SELECT "
-            ."`d`.`ID` as `i`"
-            .", `d`.`NAME` as `n`"
-            ." FROM " 
-            ."`department_user` as `du` "
-            .",`department` as `d` "
-            ." WHERE "
-            . " `du`.`id_department`=`d`.`ID` "
-            . " AND `du`.`id_user`=:id_user "
+                ."`id`"
+                .",`name`"
+                ." FROM " 
+                ."`app_task` "
+                ." WHERE "
+                . "`name`=:name "
                 ,[
-                    ':id_user'=>[$id_user,'INT']
+                    ':iname'=>[$name,'STR']
                 ]
+                ,'FETCH_OBJ'
+                ,'fetchAll'
         );
     }
-
+    public function insert(){
+        
+    }
+    public function update(){
+        
+    }
 }
