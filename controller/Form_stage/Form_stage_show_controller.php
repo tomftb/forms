ef4141e2c881e89918b50fc66c8eda561ebf74f1
tomborc <t.borczynski@autos.com.pl>
2023-10-03 15:15:07 +0200

    new controller to manage form stage data retrive from database

--- controller/Form_stage/Form_stage_show_controller.php
+++ controller/Form_stage/Form_stage_show_controller.php
@@ -17,6 +17,12 @@ final class Form_stage_show_controller extends Base_controller {
     public function __call($m,$a){
         Throw New \Exception(__METHOD__.'() Method `'.$m.'` not exists in this class `'.__CLASS__.'`!\nMethod call with arguments:\n'.serialize($a),1);
     }
+    private function setCreateUser(){
+        $this->create_user = new stdClass();
+        $this->create_user->{'create_user_login'} = $this->stage_data[0]->{'create_user_login'};
+        $this->create_user->{'create_user_email'} = $this->stage_data[0]->{'create_user_email'};
+        $this->create_user->{'create_date'} = $this->stage_data[0]->{'create_date'};
+    }
     public function getFormStageShowData(){
         $this->Log->log(0,"[".__METHOD__."]");
         //$this->Log->log(0,$_GET);
@@ -28,38 +34,32 @@ final class Form_stage_show_controller extends Base_controller {
                     'id_db'=>$this->id_db
                     ,'user'=>$this->create_user
                     ,'stage'=>$this->stage_data
+                    ,'parameters'=>$this->Controller->{'Form_stage_controller'}->getParameters()   
                 ]
         );
     }
     private function setData(){
+        $this->Log->log(0,"[".__METHOD__."]");
         self::setStage();
         self::setCreateUser();
     }    
     private function setStage(){
-        
+        $this->Log->log(0,"[".__METHOD__."]");
         $this->stage_data = $this->Model->{'Form_stage'}->get($this->id_db);
-        
         foreach($this->stage_data as &$stage){
-           
             self::setSection($stage);
         }
     }
-    private function setCreateUser(){
-        $this->create_user = new stdClass();
-        $this->create_user->{'create_user_login'} = $this->stage_data[0]->{'create_user_login'};
-        $this->create_user->{'create_user_email'} = $this->stage_data[0]->{'create_user_email'};
-        $this->create_user->{'create_date'} = $this->stage_data[0]->{'create_date'};
-    }
     private function setSection(object &$stage):void{
+        $this->Log->log(0,"[".__METHOD__."]");
         $stage->{'section'} = new stdClass();
         $stage->{'section'} = $this->Model->{'Form_stage_section'}->getByIdParent($stage->{'id_db'});
-        
         foreach($stage->{'section'} as &$section){
             self::setSubsection($section);
-            
         }
     }
     private function setSubsection(object &$section){
+        $this->Log->log(0,"[".__METHOD__."]");
         $section->{'subsection'} = new stdClass();
         $section->{'subsection'} = $this->Model->{'Form_stage_subsection'}->getByIdParent($section->{'id_db'});
         foreach($section->{'subsection'} as &$subsection){
@@ -67,25 +67,41 @@ final class Form_stage_show_controller extends Base_controller {
         }
     }
     private function setRow(object &$subsection){
+        $this->Log->log(0,"[".__METHOD__."]");
         $subsection->{'row'} = new stdClass();
-        $subsection->{'row'}->{'property'} = new stdClass();
-        $subsection->{'row'}->{'style'} = new stdClass();
         $subsection->{'row'} = $this->Model->{'Form_stage_row'}->getByIdParent($subsection->{'id_db'});
+        $this->Log->logMulti(0,$subsection->{'row'});
+        foreach($subsection->{'row'} as &$row){
+            $row->{'property'} = new stdClass();
+            $row->{'style'} = new stdClass();
+            self::{'setRow_'.$row->{'type'}}($row);
+        }
+       
     }
-    private function setRow_text(){
+    private function setRow_text(object &$row){
         
     }
-    private function setRow_input(){
+    private function setRow_input(object &$row){
         
     }
-    private function setRow_select(){
-        
+    private function setRow_select(object &$row){
+        $this->Log->log(0,"[".__METHOD__."]");
+        $row->{'glossary'} = new stdClass();
+        $row->{'glossary'} = $this->Model->{'Form_stage_row'}->getGlossaryByIdRow($row->id_db);
     }
-    private function setRow_checkbox(){
-        
+    private function setRow_checkbox(object &$row){
+        /*
+         * GET AND SET CHILD
+         */
+        $this->Log->log(0,"[".__METHOD__."]");
+        $row->{'child'} = $this->Model->{'Form_stage_row'}->getChild($row->id_db);
     }
-    private function setRow_radio(){
-        
+    private function setRow_radio(object &$row){
+        /*
+         * GET AND SET CHILD
+         */
+        $this->Log->log(0,"[".__METHOD__."]");
+        $row->{'child'} = $this->Model->{'Form_stage_row'}->getChild($row->id_db);
     }
     private function getRowchild(){
         
