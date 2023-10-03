664e68c0767a9bf9e8c1179ab252b472fd3e639e
tomborc <t.borczynski@autos.com.pl>
2023-10-03 15:00:35 +0200

    new controller to manage glossary and glossary positions

--- controller/Glossary_controller.php
+++ controller/Glossary_controller.php
@@ -10,12 +10,24 @@ final class Glossary_controller extends Base_controller {
     public function __call($m,$a){
         Throw New \Exception(__METHOD__.'() Method `'.$m.'` not exists in this class `'.__CLASS__.'`!\nMethod call with arguments:\n'.serialize($a),1);
     }
-    public function getAllWithPositions(){
-        $data = $this->Model->{'Glossary'}->getIdName();
-        foreach($data as &$glossary){
-            $glossary->{'position'}=new stdClass();
-            $glossary->{'position'} = $this->Model->{'Glossary_position'}->getIdNameByIdGlossary($glossary->{'id'});
+    public function getAllWithPositions():object{
+        (object) $data = new stdClass();
+        foreach($this->Model->{'Glossary'}->getIdName() as $glossary){
+            $data->{$glossary->id} = new stdClass();
+            $data->{$glossary->id}->{'name'} = $glossary->name;
+            $data->{$glossary->id}->{'positions'}=new stdClass();
+            $data->{$glossary->id}->{'positions'}=self::getPositions($glossary->id);
+            //$glossary->{'position'} = $this->Model->{'Glossary_position'}->getIdNameByIdGlossary($glossary->{'id'});
         }
         return $data;
+    }
+    public function getPositions(int $id_glossary=0):object{
+        (object) $positions = new stdClass();
+         foreach($this->Model->{'Glossary_position'}->getIdNameByIdGlossary($id_glossary) as $glossary){
+            $positions->{$glossary->id} = new stdClass();
+            $positions->{$glossary->id}->{'name'} = $glossary->name;
+             
+         }
+         return $positions;
     }
 }
