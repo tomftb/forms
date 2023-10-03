8c3529c0c0832852104cb8fbb39960ce685bced3
tomborc <t.borczynski@autos.com.pl>
2023-10-03 15:11:44 +0200

    add reloadFormStage(), remove new \Form_stage_create_controller() move saveFormStage() to Form_stage_create_controller()

--- controller/Form_stage/Form_stage_controller.php
+++ controller/Form_stage/Form_stage_controller.php
@@ -13,7 +13,6 @@
         $this->Model->{'Department_user'}=new \Department_user_model();
         $this->Model->{'Form_stage'}=new \Form_stage_model();
         $this->Model->{'Parametry'}=new \Parametry_model();
-        $this->Controller->{'Form_stage_create'}=new \Form_stage_create_controller();
         $this->Controller->{'Glossary'}=new \Glossary_controller();
     }
     public function __call($m,$a){
@@ -34,11 +33,15 @@
         parent::returnJson(
                 [
                     'user'=>$_SESSION
-                    ,'department'=>$this->Model->{'Department_user'}->getUserDepartments($_SESSION['userid'])
+                    ,'department'=>self::getUserDepartments()
                     ,'list'=>$this->Model->{'Form_stage'}->getList('0','0')
                     ,'glossary'=>$this->Controller->{'Glossary'}->getAllWithPositions()
                 ]
         );
+    }
+    public function reloadFormStage(){
+        $this->Log->log(0,"[".__METHOD__."]");
+        parent::returnJson($this->Model->{'Form_stage'}->getList('0','0'));
     }
     public function getFormStageList(){
         $this->Log->log(0,"[".__METHOD__."]");
@@ -48,12 +51,20 @@
         $this->Log->log(0,"[".__METHOD__."]");
         parent::returnJson(
                 [
-                    'department'=>$this->Model->{'Department_user'}->getUserDepartments($_SESSION['userid'])
-                    ,'parameters'=>self::getParameters()       
+                    //'department'=>$this->Model->{'Department_user'}->getUserDepartments($_SESSION['userid'])
+                    'parameters'=>self::getParameters()       
                 ]
         );
     }
-    private function getParameters(){
+    private function getUserDepartments(){
+        (object)$departments = new stdClass();
+        foreach($this->Model->{'Department_user'}->getUserDepartments($_SESSION['userid']) as $department){
+            $departments->{$department->{'id'}} = $department->{'name'}; //new stdClass();
+            //$departments->{$department->{'id'}}->{'name'}=$department->{'name'};
+        }
+        return $departments;
+    }
+    public function getParameters(){
         (object) $parameters = new stdClass();
         $throw='throwError';
         foreach($this->Model->{'Parametry'}->getFormStageCreate() as $parm){
@@ -63,19 +74,17 @@
         parent::{$throw}(__METHOD__.' No \'FORM_STAGE\' parameters in database!',1);
         return $parameters;
     }
-
-    public function saveFormStage(){
-        $this->Log->log(0,"[".__METHOD__."]");
-        (array) $post = json_decode(filter_input(INPUT_POST,'data'));
-        if(json_last_error()){
-            throw new \exception(json_last_error_msg(),0);
-        }
-        $this->Controller->{'Form_stage_create'}->create($post);
-        $response = $this->Controller->{'Form_stage_create'}->get();
-        $response->id_db = 0;
-        parent::returnJson($response);
-    }
     public function exists(int $id_db=0):void{
         $this->Model->{'Form_stage'}->exists($id_db);
+    }
+    public function getStageUser(object $Stage):object{
+        (object) $User = new stdClass();
+        $User->{'create_date'} = $Stage->create_date;
+        $User->{'create_user_login'} = $Stage->create_user_login;
+        $User->{'create_user_email'} = $Stage->create_user_email;
+        $User->{'mod_user_login'} = $Stage->mod_user_login;
+        $User->{'mod_user_email'} = $Stage->mod_user_email;
+        $User->{'mod_date'} = $Stage->mod_date;
+        return $User;
     }
 }
