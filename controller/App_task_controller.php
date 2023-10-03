ec4c8af59aab363916c444f8889d0779a542dbc8
tomborc <t.borczynski@autos.com.pl>
2023-10-03 15:03:51 +0200

    add new prototype of controller to manage data app_task and slo in database

--- controller/App_task_controller.php
+++ controller/App_task_controller.php
@@ -1,21 +1,19 @@
 <?php
-final class Glossary_controller extends Base_controller {
+final class App_task_controller extends Base_controller {
 
     public function __construct(){
         parent::__construct();
         $this->Log->log(0,"[".__METHOD__."]");
-        $this->Model->{'Glossary'}=new \Glossary_model();
-        $this->Model->{'Glossary_position'}=new \Glossary_position_model();
+        $this->Model->{'App_task'}=new \App_task_model();
+        $this->Model->{'Slo'}=new \Slo_model();
     }
     public function __call($m,$a){
         Throw New \Exception(__METHOD__.'() Method `'.$m.'` not exists in this class `'.__CLASS__.'`!\nMethod call with arguments:\n'.serialize($a),1);
     }
-    public function getAllWithPositions(){
-        $data = $this->Model->{'Glossary'}->getIdName();
-        foreach($data as &$glossary){
-            $glossary->{'position'}=new stdClass();
-            $glossary->{'position'} = $this->Model->{'Glossary_position'}->getIdNameByIdGlossary($glossary->{'id'});
-        }
-        return $data;
+    public function getAllWithPositions():object{
+
+    }
+    public function getPositions(int $id_glossary=0):object{
+        
     }
 }
