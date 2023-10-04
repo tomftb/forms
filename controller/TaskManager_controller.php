7fa87432f93f97de6995a09de5c774a700236814
tomborc <t.borczynski@autos.com.pl>
2023-10-04 08:59:13 +0200

    add new controller to list this->modul, add new method findMethod(), checkMethod(), setMethodName() to split code in loadMethod()

--- controller/TaskManager_controller.php
+++ controller/TaskManager_controller.php
@@ -65,15 +65,16 @@ class Invoker
 }
 final class TaskManager_controller{
     
-    private $modulData;
     private $modul=[];
     private $Log;
+    private ?array $methodFound=[];
+    private ?int $methodFoundCounter=0;
+    private ?string $method='';
     
     public function __construct(){
         $this->Log = Logger::init(__FILE__);
         self::loadModules();
     }
-    
     public function loadModules(){
         $this->Log->log(0,"[".__METHOD__."]");
         array_push($this->modul,new ManageParameters());
@@ -97,35 +98,47 @@ final class TaskManager_controller{
         array_push($this->modul,new \Form_stage_show_controller());
         array_push($this->modul,new \Form_stage_edit_controller());
         array_push($this->modul,new \Form_stage_create_controller());
+        array_push($this->modul,new \Form_creator_controller());
     }
-    
-    public function loadMethod($method=''){
+    public function loadMethod(mixed $method):void{
         $this->Log->log(0,"[".__METHOD__."]");
-        $methodFound=[];
-        $methodFoundCounter=0;
+        self::setMethodName($method);
+        self::findMethod();
+        self::checkMethod();
+        /*
+         * RUN CLASS METHOD
+         */
+        $invoker = new \Invoker();
+        /*
+         * $this->modul[$methodFound[0]] =>  already new Obkect()
+         */
+        $invoker->setTask(New Task($this->modul[$this->methodFound[0]],$this->method));
+        $invoker->runTask();
+    }
+    private function findMethod():void{
         foreach($this->modul as $id => $name){
-            if(method_exists ( $name , $method )){
-                array_push($methodFound,$id);
-                $this->Log->log(0,"[".__METHOD__."] METHOD FOUND IN CLASS => ".get_class($name));
-                $methodFoundCounter++;
+            if(method_exists ( $name , $this->method )){
+                array_push($this->methodFound,$id);
+                $this->Log->log(0,"[".__METHOD__."] Method `". $this->method."` found in class `".get_class($name)."`");
+                $this->methodFoundCounter++;
             }
         }
-        if($methodFoundCounter===0){
-            throw New Exception('Task not exists => '.$method,1);
+    }
+    private function checkMethod(){
+        if($this->methodFoundCounter===0){
+            throw New \Exception("[".__METHOD__."] Task not exists `".$this->method."` add controller with method via loadModules()?",1);
         }
-        else if($methodFoundCounter>1){
-            throw New Exception('Task avaliable in more than one model => '.$method,1);
+        if($this->methodFoundCounter>1){
+            throw New \Exception("[".__METHOD__."] Task avaliable in more than one model `".$this->method."`",1);
+        } 
+    }
+    private function setMethodName(mixed $method):void{
+        if(gettype($method)!=='string'){
+             throw New \Exception("[".__METHOD__."] Method name is not a string !",1);
         }
-        else{
-            /*
-             * RUN CLASS METHOD
-             */
-            $invoker = new Invoker();
-            /*
-             * $this->modul[$methodFound[0]] =>  already new Obkect()
-             */
-            $invoker->setTask(New Task($this->modul[$methodFound[0]],$method));
-            $invoker->runTask();
+        $this->method=trim($method);
+        if($this->method===''){
+             throw New \Exception("[".__METHOD__."] Method name `".$this->method."` is empty string trim()!",1);
         }
     }
     public function __destruct(){}
