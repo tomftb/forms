65fcd5291fafaef3dd2ce91251805a8bfb804566
tomborc <t.borczynski@autos.com.pl>
2023-10-10 07:32:05 +0200

    new controller to receive users forms

--- controller/Form_creator/Form_creator_controller.php
+++ controller/Form_creator/Form_creator_controller.php
@@ -5,21 +5,33 @@
     public function __construct(){
         parent::__construct();
         $this->Log->log(0,"[".__METHOD__."]");
-        //$this->Model->{'Uprawnienia'}=new \Uprawnienia_model();
-        //$this->Model->{'Department_user'}=new \Department_user_model();
         $this->Model->{'Form_stage'}=new \Form_stage_model();
-       // $this->Model->{'Parametry'}=new \Parametry_model();
-       // $this->Controller->{'Glossary'}=new \Glossary_controller();
+        $this->Controller->{'Department'}=new \Department_controller();
+        $this->Controller->{'Form_stage_get'} = new  \Form_stage_get_controller();
+        $this->Controller->{'Form_creator_get'} = new  \Form_creator_get_controller();
+        $this->Controller->{'Glossary'}=new \Glossary_controller();
     }
     public function __call($m,$a){
         Throw New \Exception(__METHOD__.'() Method `'.$m.'` not exists in this class `'.__CLASS__.'`!\nMethod call with arguments:\n'.serialize($a),1);
     }
-    public function getFormCreator(){
+    public function getFormStagesList(){
         $this->Log->log(0,"[".__METHOD__."]");
-        //return '';
         parent::returnJson(
                 [
-                    'stage'=>$this->Model->{'Form_stage'}->getList()
+                    'stage'=>$this->Controller->{'Form_stage_get'}->getAllNoId()
+                    ,'department'=>$this->Controller->{'Department'}->getUserDepartments()
+                    ,'glossary'=>$this->Controller->{'Glossary'}->getAllWithPositions()
+                ]
+        );
+    }
+    public function getFormDescription(){
+        $this->Log->log(0,"[".__METHOD__."]");
+        parent::returnJson(
+                [
+                    'stage'=>$this->Controller->{'Form_stage_get'}->getAllNoId()
+                    ,'department'=>$this->Controller->{'Department'}->getUserDepartments()
+                    ,'glossary'=>$this->Controller->{'Glossary'}->getAllWithPositions()
+                    ,'form'=>$this->Controller->{'Form_creator_get'}->get(filter_input(INPUT_GET,'id',FILTER_VALIDATE_INT))
                 ]
         );
     }
