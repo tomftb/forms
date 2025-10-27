ead835fdc67b8435dbc33c1c6be95cb2224527b8
Tomasz-plasticon <t.borczynski@plasticoncomposites.com>
2025-10-27 12:21:48 +0100

    update methods compatibility with Iterator

--- class/DataIterator.php
+++ class/DataIterator.php
@@ -10,22 +10,22 @@ class DataIterator implements \Iterator
         self::fill($data);
         $this->rewind();
     }
-    public function rewind(){
+    public function rewind():void{
         reset($this->values);
     }
 
-    public function valid(){
+    public function valid():bool{
         return false !== $this->current();
     }
 
-    public function next(){
+    public function next():void{
         next($this->values);
     }
 
-    public function current(){
+    public function current():mixed{
         return current($this->values);
     }
-    public function key(){
+    public function key():mixed{
         return key($this->values);
     }
 	public function unFill(){
